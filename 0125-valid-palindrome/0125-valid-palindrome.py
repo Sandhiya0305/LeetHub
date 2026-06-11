0cdf905973ffdb5f2b9a8043e5f4c7983ce190f5
SANDHIYA R <sandhiyarajkumar0305@gmail.com>
2026-06-11 20:12:29 +0530

    Time: 3 ms (99%), Space: 20.2 MB (29.4%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.py
+++ 0125-valid-palindrome/0125-valid-palindrome.py
@@ -1,7 +1,4 @@
 class Solution:
     def isPalindrome(self, s: str) -> bool:
-        r = ""
-        s = s.lower()
-        for i in s:
-            if i.isalnum(): r += i
+        r = "".join(filter(str.isalnum, s)).lower()
         return r == r[::-1]
