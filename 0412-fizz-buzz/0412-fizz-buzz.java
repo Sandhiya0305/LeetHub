6b843a18d38dd6de0c7310932ef202f773404bb0
SANDHIYA R <sandhiyarajkumar0305@gmail.com>
2026-06-29 12:10:24 +0530

    Time: 1 ms (99.87%), Space: 46.9 MB (45.69%) - LeetHub

---
+++ 0412-fizz-buzz/0412-fizz-buzz.java
@@ -0,0 +1,9 @@
+class Solution {
+    public List<String> fizzBuzz(int n) {
+        List<String> answer = new ArrayList<>();
+
+        for(int i = 1; i <= n; i++) answer.add((i % 3 == 0 && i % 5 == 0)? "FizzBuzz" : ((i % 3 == 0)? "Fizz": (i % 5 == 0)? "Buzz" : Integer.toString(i)));
+
+        return answer;
+    }
+}
